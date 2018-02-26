1239692b8d688fa2ec3277e3791bd9e3f720cf3f
Zubin Dodhi <zubindodhi@Zubins-MacBook-Pro.local>
2018-02-26 09:11:00 -1000

    react creating store and whatwg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider, connect } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+// import reducers from "./reducers"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const boundCompose = compose.bind(null, applyMiddleware());
+
+const store = createStore(
+  // reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? boundCompose(window.__REDUX_DEVTOOLS_EXTENSION__())
+    : boundCompose()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
